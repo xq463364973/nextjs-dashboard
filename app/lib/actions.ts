01228c92bb14f5db7fde10fded2c1343b0dd2795
maxq3 <maxq3@asiainfo.com>
2024-09-24 11:06:41 +0800

    fix ESLint

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -78,7 +78,8 @@ export async function createInvoice(prevState: State, formData: FormData) {
         INSERT INTO invoices (customer_id, amount, status, date)
         VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
       `;
-    } catch (error) {
+    } catch (_error) {
+      console.error(_error);
       // If a database error occurs, return a more specific error.
       return {
         message: 'Database Error: Failed to Create Invoice.',
@@ -106,7 +107,8 @@ export async function updateInvoice(id: string, formData: FormData) {
             SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
             WHERE id = ${id}
           `;
-      } catch (error) {
+      } catch (_error) {
+        console.error(_error);
         return { message: 'Database Error: Failed to Update Invoice.' };
       }
    
@@ -116,12 +118,13 @@ export async function updateInvoice(id: string, formData: FormData) {
 
   // 删除
   export async function deleteInvoice(id: string) {
-    throw new Error('Failed to Delete Invoice');
+    // throw new Error('Failed to Delete Invoice');
     try {
         await sql`DELETE FROM invoices WHERE id = ${id}`;
         revalidatePath('/dashboard/invoices');
         return { message: 'Deleted Invoice.' };
-      } catch (error) {
+      } catch (_error) {
+        console.error(_error);
         return { message: 'Database Error: Failed to Delete Invoice.' };
       }
   }
